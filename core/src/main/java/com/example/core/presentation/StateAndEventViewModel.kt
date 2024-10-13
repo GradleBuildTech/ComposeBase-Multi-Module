210aeab49b658d1bbba35e9f014e9d8c23044c3c
Nguyễn Minh Hưng <90996598+nguyenminhhung2011@users.noreply.github.com>
2024-10-13 15:39:18 +0700

    ✨ feat: Implement initialData response for base viewModel

--- core/src/main/java/com/example/core/presentation/StateAndEventViewModel.kt
+++ core/src/main/java/com/example/core/presentation/StateAndEventViewModel.kt
@@ -2,7 +2,12 @@ package com.example.core.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.core.models.stateData.Either
+import com.example.core.models.stateData.ExceptionState
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -22,6 +27,7 @@ abstract class StateAndEventViewModel<UiState, Event>(initialState: UiState) : V
     private val events = MutableSharedFlow<Event>(replay = 0)
     private val _uiState: MutableStateFlow<UiState> = MutableStateFlow(initialState)
     val uiState: StateFlow<UiState> = _uiState.asStateFlow()
+
     init {
         viewModelScope.launch {
             events.collect { event -> handleEvent(event) }
@@ -36,6 +42,56 @@ abstract class StateAndEventViewModel<UiState, Event>(initialState: UiState) : V
 
     fun onEvent(event: Event) {
         viewModelScope.launch { events.emit(event) }
+    }
+
+    ///✨===============================================
+    ///[loadInitialData] is a suspend function that loads the initial data.
+    ///[initialLoadState] is a lambda function that returns the initial state of the view model.
+    ///[errorLoadState] is a lambda function that returns the error state of the view model.
+    ///[listRequest] is a list of flow of either exception state or any.
+
+    ///[listResponse] is a lambda function that takes in the response and response index.
+    ///* (response, responseIndex) -> Unit
+    ///* response is the response of the request.
+    ///* responseIndex is the index of the response.
+
+    ///Example:
+    ///```kotlin
+    ///loadInitialData(
+    ///    initialLoadState = { copy(isLoading = true) },
+    ///    errorLoadState = { _ -> copy(isLoading = false) },
+    ///    listRequest = listOf(
+    ///        homeUseCase.fetchRecommendedCourses(createPaginationRequest(Constants.COURSE_LIMIT_ITEM)),
+    ///        homeUseCase.fetchTutors(createPaginationRequest(Constants.TUTOR_LIMIT_ITEM)),
+    ///        homeUseCase.fetchEBooks(createPaginationRequest(Constants.EBOOK_LIMIT_ITEM))
+    ///    )
+    ///) { response, responseIndex ->
+    ///    when (responseIndex) {
+    ///        0 -> updateStateWithCourses(response.toListEither())
+    ///        1 -> updateStateWithTutors(response.toListEither())
+    ///        2 -> updateStateWithEBooks(response.toListEither())
+    ///    }
+    fun loadInitialData(
+        initialLoadState: UiState.() -> UiState,
+        errorLoadState: UiState.(String) -> UiState,
+        listRequest: List<Flow<Either<ExceptionState, Any>>>,
+
+        listResponse: (Either<ExceptionState, Any>, Int) -> Unit
+    ) {
+        setUiState { _uiState.value.initialLoadState() }
+        val request = listRequest.map { req -> viewModelScope.async { req } }
+
+        viewModelScope.launch {
+            try {
+                ///✨===============================================
+                ///[awaitAll] is a suspend function that waits for all the given deferred values to complete.
+                val results = awaitAll(*request.toTypedArray())
+                results.forEachIndexed { index, rp -> rp.collect { listResponse(it, index) } }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                setUiState { _uiState.value.errorLoadState(e.message ?: "") }
+            }
+        }
     }
 
     ///✨===============================================
--- feat/src/main/java/com/example/feat/home/controller/HomeViewModel.kt
+++ feat/src/main/java/com/example/feat/home/controller/HomeViewModel.kt
@@ -1,104 +1,70 @@
 package com.example.feat.home.controller
 
-import androidx.lifecycle.viewModelScope
+import com.example.core.extensions.checkItemsAre
+import com.example.core.extensions.toListEither
 import com.example.core.lib.constants.Constants
 import com.example.core.models.pagination.PaginationRequest
+import com.example.core.models.stateData.Either
+import com.example.core.models.stateData.ExceptionState
 import com.example.core.presentation.StateAndEventViewModel
-import com.example.domain.entity.CourseEntity
-import com.example.domain.entity.EBookEntity
-import com.example.domain.entity.TutorEntity
 import com.example.domain.usecase.home.HomeUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val homeUseCase: HomeUseCase,
-) : StateAndEventViewModel<HomeState, HomeEvent>(initialState = HomeState()){
-    override suspend fun handleEvent(event: HomeEvent) {
-        when(event){
-            is HomeEvent.LoadTutors -> loadTutors(paginationRequest = event.paginationRequest)
-            is HomeEvent.LoadRecommendedCourses -> loadRecommendedCourses(paginationRequest = event.paginationRequest)
-            is HomeEvent.LoadEBooks -> loadEBooks(paginationRequest = event.paginationRequest)
-        }
-    }
+    homeUseCase: HomeUseCase,
+) : StateAndEventViewModel<HomeState, HomeEvent>(initialState = HomeState()) {
 
     init {
-        onEvent(
-            HomeEvent.LoadTutors(
-                paginationRequest = PaginationRequest(
-                    page = 1,
-                    pageSize = Constants.TUTOR_LIMIT_ITEM
-                )
+        loadInitialData(
+            initialLoadState = { copy(isLoading = true) },
+            errorLoadState = { _ -> copy(isLoading = false) },
+            listRequest = listOf(
+                homeUseCase.fetchRecommendedCourses(createPaginationRequest(Constants.COURSE_LIMIT_ITEM)),
+                homeUseCase.fetchTutors(createPaginationRequest(Constants.TUTOR_LIMIT_ITEM)),
+                homeUseCase.fetchEBooks(createPaginationRequest(Constants.EBOOK_LIMIT_ITEM))
             )
-        )
-        onEvent(
-            HomeEvent.LoadRecommendedCourses(
-                paginationRequest = PaginationRequest(
-                    page = 1,
-                    pageSize = Constants.COURSE_LIMIT_ITEM
-                )
-            )
-        )
-        onEvent(
-            HomeEvent.LoadEBooks(
-                paginationRequest = PaginationRequest(
-                    page = 1,
-                    pageSize = Constants.EBOOK_LIMIT_ITEM
-                )
-            )
-        )
-    }
-
-    private fun loadTutors(paginationRequest: PaginationRequest) {
-        if (uiState.value.isLoadingTutors) return
-
-        viewModelScope.launch {
-            setUiState { copy(isLoadingTutors = true) }
-            homeUseCase.fetchTutors(paginationRequest = paginationRequest)
-                .collect { either ->
-                    if (either.isRight()) {
-                        val tutors = either.rightValue()
-                        setUiState { copy(isLoadingTutors = false, tutors = tutors as List<TutorEntity>) }
-                    } else if (either.isLeft()) {
-                        setUiState { copy(isLoadingTutors = false) }
-                    }
-                }
+        ) { response, responseIndex ->
+            when (responseIndex) {
+                0 -> updateStateWithCourses(response.toListEither())
+                1 -> updateStateWithTutors(response.toListEither())
+                2 -> updateStateWithEBooks(response.toListEither())
+            }
         }
     }
 
-    private fun loadRecommendedCourses(paginationRequest: PaginationRequest) {
-        if (uiState.value.isLoadingRecommendedCourses) return
+    override suspend fun handleEvent(event: HomeEvent) {
+    }
 
-        viewModelScope.launch {
-            setUiState { copy(isLoadingRecommendedCourses = true) }
-            homeUseCase.fetchRecommendedCourses(paginationRequest = paginationRequest)
-                .collect { either ->
-                    if (either.isRight()) {
-                        val recommendedCourses = either.rightValue()
-                        setUiState { copy(isLoadingRecommendedCourses = false, recommendedCourses = recommendedCourses as List<CourseEntity>) }
-                    } else if (either.isLeft()) {
-                        setUiState { copy(isLoadingRecommendedCourses = false) }
-                    }
-                }
+    private fun createPaginationRequest(limit: Int) = PaginationRequest(page = 1, pageSize = limit)
+
+
+    private fun updateStateWithCourses(either: Either<ExceptionState, List<Any>>) {
+        if (either.isRight()) {
+            setUiState {
+                copy(
+                    recommendedCourses = either.rightValue()?.checkItemsAre() ?: emptyList()
+                )
+            }
         }
     }
 
-    private fun loadEBooks(paginationRequest: PaginationRequest) {
-        if (uiState.value.isLoadingEBooks) return
-
-        viewModelScope.launch {
-            setUiState { copy(isLoadingEBooks = true) }
-            homeUseCase.fetchEBooks(paginationRequest = paginationRequest)
-                .collect { either ->
-                    if (either.isRight()) {
-                        val eBooks = either.rightValue()
-                        setUiState { copy(isLoadingEBooks = false, eBooks = eBooks as List<EBookEntity>) }
-                    } else if (either.isLeft()) {
-                        setUiState { copy(isLoadingEBooks = false) }
-                    }
-                }
+    private fun updateStateWithEBooks(either: Either<ExceptionState, List<Any>>) {
+        if (either.isRight()) {
+            setUiState {
+                copy(
+                    eBooks = either.rightValue()?.checkItemsAre() ?: emptyList(),
+                    isLoading = false
+                )
+            }
         }
     }
+
+    private fun updateStateWithTutors(either: Either<ExceptionState, List<Any>>) {
+        if (either.isRight()) {
+            setUiState { copy(tutors = either.rightValue()?.checkItemsAre() ?: emptyList()) }
+        }
+    }
+
 }
